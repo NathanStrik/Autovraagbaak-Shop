9dedf468a3707050436e2884ed5b442fbf9a7f84
Nathan Strik <contact@nathanstrik.nl>
2023-08-29 22:06:12 +0200

    Improved HTTP Error Handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import booksRoutes from "./routes/books";
 import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 
 const LOGGING_LEVEL = "dev";
 
@@ -14,15 +15,19 @@ app.use(express.json());
 app.use("/api/books", booksRoutes);
 
 app.use((req, res, next) => {
-	next(Error("Endpoint not found."));
+	next(createHttpError(404, "Endpoint not found."));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
 	console.error(error);
+	let statusCode = 500;
 	let errorMessage = "An unknown error occured";
-	if (error instanceof Error) errorMessage = error.message;
-	res.status(500).json({ error: errorMessage });
+	if (isHttpError(error)) {
+		statusCode = error.status;
+		errorMessage = error.message;
+	}
+	res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
