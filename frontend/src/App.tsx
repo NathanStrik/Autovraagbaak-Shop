6894ed505d6c9eeeca9d7e6aa480159feb554efd
Nathan Strik <contact@nathanstrik.nl>
2023-09-02 18:34:58 +0200

    Basic React frontend with button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [counter, setCounter] = useState(0);
+
+	return (
+		<div className="App">
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<p>
+				Hello, this is basic UI!
+			</p>
+			<Button onClick={() => setCounter(counter + 1)}>
+				Clicked {counter} times
+			</Button>
+		</header>
+		</div>
+	);
 }
 
 export default App;
